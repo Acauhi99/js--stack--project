330c716ae0cdb4d1d317b52789520e82130f0d79
Mateus Acauhi <mateusacauhi@bluems.com>
2025-04-09 18:17:51 -0300

    feat: dockenerizar a infra da api

--- api/src/@domain/todo/todo.service.ts
+++ api/src/@domain/todo/todo.service.ts
@@ -18,21 +18,22 @@ export class TodoService {
 
   async findOne(id: string): Promise<Todo> {
     const todo = await this.todoRepository.findOne({ where: { id } });
+
     if (!todo) {
       throw new NotFoundException(`Todo with ID ${id} not found`);
     }
+
     return todo;
   }
 
   async create(createTodoDto: TodoCreateFieldsDto): Promise<Todo> {
     const todo = this.todoRepository.create(createTodoDto);
+
     return this.todoRepository.save(todo);
   }
 
   async update(id: string, updateTodoDto: TodoUpdateFieldsDto): Promise<Todo> {
     const todo = await this.findOne(id);
-
-    // Apply updates to the todo
     Object.assign(todo, updateTodoDto);
 
     return this.todoRepository.save(todo);
@@ -40,6 +41,7 @@ export class TodoService {
 
   async remove(id: string): Promise<void> {
     const result = await this.todoRepository.delete(id);
+
     if (result.affected === 0) {
       throw new NotFoundException(`Todo with ID ${id} not found`);
     }
